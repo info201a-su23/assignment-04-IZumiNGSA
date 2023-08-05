c4c6aedf32bbd914ee237c6cad695ef421787b97
ChufengWang <137837670+IZumiNGSA@users.noreply.github.com>
2023-08-04 23:18:27 -0700

    Update summary.R

---
+++ summary.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(readr)
+
+washington <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-jail-rates-1990-WA.csv")
+
+# Average variables in current year
+avg_jail_2018 <- washington %>%
+  filter(year == 2018) %>%
+  summarise(mean = mean(total_jail_pop_rate)) %>%
+  pull(mean)
+
+avg_jail_2018_native <- washington %>%
+  filter(year == 2018) %>%
+  summarise(mean = mean(native_jail_pop_rate)) %>%
+  pull(mean)
+
+avg_jail_2018_aapi <- washington %>%
+  filter(year == 2018) %>%
+  summarise(mean = mean(aapi_jail_pop_rate)) %>%
+  pull(mean)
+
+avg_jail_2018_latinx <- washington %>%
+  filter(year == 2018) %>%
+  summarise(mean = mean(latinx_jail_pop_rate)) %>%
+  pull(mean)
+
+max_jail_rate_native <- washington %>%
+  filter(year == 2018) %>%
+  filter(native_jail_pop_rate == max(native_jail_pop_rate, na.rm = TRUE)) %>%
+  pull(native_jail_pop_rate)
+
+max_jail_rate_aapi <- washington %>%
+  filter(year == 2018) %>%
+  filter(aapi_jail_pop_rate == max(aapi_jail_pop_rate, na.rm = TRUE)) %>%
+  pull(aapi_jail_pop_rate)
+
+max_jail_rate_latinx <- washington %>%
+  filter(year == 2018) %>%
+  filter(latinx_jail_pop_rate == max(latinx_jail_pop_rate, na.rm = TRUE)) %>%
+  pull(latinx_jail_pop_rate)
+
+observations <- nrow(washington)
+
+features <- ncol(washington)
