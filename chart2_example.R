222b7b6ac93961cd1b4282d29be7f97768a56818
ChufengWang <137837670+IZumiNGSA@users.noreply.github.com>
2023-08-04 23:18:53 -0700

    Update chart2_example.R

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,13 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(readr)
+
+washington <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-jail-rates-1990-WA.csv")
+
+washington <- na.omit(washington)
+ggplot(washington, aes(x = native_jail_pop_rate, y = native_prison_pop_rate)) +
+  geom_point() +
+  theme_minimal() +
+  labs(title = "Relationship Between Native Jail and Prison Population Rates from 1990-2018",
+       y = "Native Prison Population Rate",
+       x = "Native Jail Population Rate") +
+  xlim(0,10000)
